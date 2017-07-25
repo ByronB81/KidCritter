65acd450f133536ac90161fded68399579c85ef7
Byron Blocker <byronblocker@gmail.com>
2017-07-25 01:49:01 -0700

    Add project files.

---
+++ KidCritter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace KidCritter
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Pick a number 1-10");
+            string strNum = Console.ReadLine();
+            int userNum = int.Parse(strNum);
+
+            Random rnd = new Random();
+            int firstGuess = rnd.Next(1,10);
+            Console.WriteLine("My guess is {0}", firstGuess);
+
+            Console.WriteLine("Your number is {0}", userNum);
+        }
+    }
+}
